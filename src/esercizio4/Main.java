c49a759ea27dce4bc9ba82fda6f8c858d8ceb83a
Noemi <noemibarone00@gmail.com>
2023-04-06 16:08:02 +0200

    esercizio 4

---
+++ src/esercizio4/Main.java
@@ -0,0 +1,107 @@
+package esercizio4;
+
+public class Main {
+
+    // Don't forget to use comments to clear things up if you need them!
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4Bonus();
+        exercise5Bonus();
+    }
+
+
+
+    /**
+     * 1: Create an int variable, add 1 to the value and then print it. Use the + operator
+     * Then minus 1 from the same variable and print it.
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        int variable = 10;
+        variable = variable + 1;
+        System.out.println(variable);
+        variable = variable - 1;
+        System.out.println(variable);
+
+    }
+
+    /**
+     * 2: Create a double variable, times by 2 and then print it.
+     * <p>
+     * Create another double variable, which is the previous variable divided by 2
+     * and then print it
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        double number1 = 24.25;
+        number1 = number1 * 2;
+        System.out.println(number1);
+        double number2 = 24.25;
+        number2 = number2 / 2;
+        System.out.println(number2);
+    }
+
+    /**
+     * 3: We are going to convert the temperature from celsius to fahrenheit
+     * <p>
+     * Create a double variable called celsius
+     * <p>
+     * Create a new variable called fahrenheit
+     * <p>
+     * The variable fahrenheit should be set as
+     * celsius times by 1.8 plus 32
+     * <p>
+     * Print out celsius and fahrenheit with 2 print statements
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+        double celsius = 1.8;
+        double fahrenheit = celsius + 32;
+        System.out.println("Celsius: " + celsius);
+        System.out.println("Fahreneheit: " + fahrenheit);
+    }
+
+    /**
+     * 4: BONUS! This is an optional task, but it is recommended you complete it
+     * ----------------------------------------------------------------------
+     * Create an int called age with a starting value of 10.
+     * <p>
+     * Only using the '++' operator increase this value to 15
+     */
+    private static void exercise4Bonus() {
+        System.out.println("\nExercise 4:");
+        int value = 10;
+        value++;
+        value++;
+        value++;
+        value++;
+        value++;
+        System.out.println(value);
+    }
+
+    /**
+     * 5: BONUS! This is an optional task, but it is recommended you complete it
+     * ----------------------------------------------------------------------
+     * Create an int called age set as 50
+     * <p>
+     * Using the -= operator change this value from 50 to 0 in one line of code,
+     * <p>
+     * Underneath create another int called secondAge set as 50
+     * <p>
+     * Change this value from 50 to 0 in 3 lines of code
+     */
+    private static void exercise5Bonus() {
+        System.out.println("\nExercise 5:");
+        int age = 50;
+        age-= 50;
+        System.out.println(age);
+        int secondAge = 50;
+        secondAge -= 25;
+        secondAge -= 20;
+        secondAge -= 5;
+        System.out.println(secondAge);
+    }
+}
+
